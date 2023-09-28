5f37bea9dfdd779bba2c0f581c0c7a96de9b473f
cecilia <cecilia.martial@hotmail.com>
2023-09-28 16:17:51 +0200

    commit correctif ok

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 //const cookieParser = require('cookie-parser');
 // const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+//const indexRouter = require('./routes/index');
 
 // Importation du fichier de route user.js
 const usersRouter = require('./routes/users');
@@ -23,34 +23,23 @@ connectDB();
 app.set('view engine', 'twig');
 app.set('views', './views');
 app.use(express.static('public'));
+//Ajouter le middleware 'body-parser'
+app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+// catch 404 and forward to error handler
 
 // app.use(logger('dev'));
  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.urlencoded({ extended: true }));
 // app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 //configuration des routes
 app.get('/', (req, res) => {
-  res.render('pages/home.twig',{title:"TP PROJECT MANAGER"});
+  res.render('pages/home.twig',{title:"TP PROJECT MANAGER DE CECILIA"});
 });
     // Utilisation des routes définies dans user.js
     app.use('/', usersRouter);
@@ -60,3 +49,18 @@ app.listen(5555, () => {
 });
 
 module.exports = app;
+
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
